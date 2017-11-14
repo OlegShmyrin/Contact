3e74b98ab6cf007fc19d3a821e6489c098617d10
Oleg.Shmyrin <Oleg.shmyrin@gmail.com>
2017-11-14 16:50:03 +0200

    Initial commit

---
+++ src/dao/ContactDAO.java
@@ -0,0 +1,16 @@
+package dao;
+import entity.Contact;
+import java.util.List;
+
+public interface ContactDAO {
+    // Добавление контакта - возвращает ID добавленного контакта
+    public Long addContact(Contact contact);
+    // Редактирование контакта
+    public void updateContact(Contact contact);
+    // Удаление контакта по его ID
+    public void deleteContact(Long contactId);
+    // Получение контакта
+    public Contact getContact(Long contactId);
+    // Получение списка контактов
+    public List<Contact> findContacts();
+}
